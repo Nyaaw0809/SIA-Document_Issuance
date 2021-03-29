463aee07198a768fd3c668eb03eed42ce1e5378b
BSIT-RannyEdejer <78744482+BSIT-RannyEdejer@users.noreply.github.com>
2021-03-29 22:45:45 +0800

    pdf of indigency and residency

--- src/app/residency-pdf/residency-pdf.component.ts
+++ src/app/residency-pdf/residency-pdf.component.ts
@@ -14,6 +14,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 })
 export class ResidencyPdfComponent implements OnInit {
 
+  myDate = Date.now();
   lastn: string;
   firstn: string;
   midn: string;
